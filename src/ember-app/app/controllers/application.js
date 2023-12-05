8285ab11721996f0335f50a1a7e21b1af13b8a3d
Flexberry-man <mail@flexberry.net>
2023-12-05 10:50:10 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/application.js
+++ src/ember-app/app/controllers/application.js
@@ -28,25 +28,25 @@ export default Controller.extend({
             link: 'i-i-s-proba-11-город-l',
             caption: i18n.t('forms.application.sitemap.proba-11.i-i-s-proba-11-город-l.caption'),
             title: i18n.t('forms.application.sitemap.proba-11.i-i-s-proba-11-город-l.title'),
-            icon: 'chart line',
+            icon: 'address card',
             children: null
           }, {
             link: 'i-i-s-proba-11-улица-l',
             caption: i18n.t('forms.application.sitemap.proba-11.i-i-s-proba-11-улица-l.caption'),
             title: i18n.t('forms.application.sitemap.proba-11.i-i-s-proba-11-улица-l.title'),
-            icon: 'building',
+            icon: 'briefcase',
             children: null
           }, {
             link: 'i-i-s-proba-11-номер-дом-l',
             caption: i18n.t('forms.application.sitemap.proba-11.i-i-s-proba-11-номер-дом-l.caption'),
             title: i18n.t('forms.application.sitemap.proba-11.i-i-s-proba-11-номер-дом-l.title'),
-            icon: 'tags',
+            icon: 'chart line',
             children: null
           }, {
             link: 'i-i-s-proba-11-этаж-l',
             caption: i18n.t('forms.application.sitemap.proba-11.i-i-s-proba-11-этаж-l.caption'),
             title: i18n.t('forms.application.sitemap.proba-11.i-i-s-proba-11-этаж-l.title'),
-            icon: 'phone',
+            icon: 'chart line',
             children: null
           }]
         }
